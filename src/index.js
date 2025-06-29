c14ae3d277fb51b55548d5759a7771196213738f
Nelson jr <nelsonjr.dev@gmail.com>
2025-06-29 17:05:51 -0300

    feat: add global style with background color

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { App } from './pages';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,11 @@
-export const App = () => 'Meu app';
+import * as React from 'react'
+import { Theme } from './../components/Theme'
+import { Signup } from './Signup'
+
+export const App = () => {
+  return (
+    <Theme>
+     <Signup />
+    </Theme>
+  )
+}
