ba5f79db56e828f65072b64898d636a60b78a3c1
Nelson jr <nelsonjr.dev@gmail.com>
2025-07-01 19:04:13 -0300

    feat: basic signup form

--- src/components/uikit/Input/index.js
+++ src/components/uikit/Input/index.js
@@ -7,6 +7,7 @@ export const Input = styled('input')`
   border: 1px solid #fff;
   border-radius: 200px;
   color: ${props => th.color('white')(props)};
-  padding: ${th.space(2)}px ${th.space(1)}px;
+  padding: ${th.space(2)}px ${th.space(3)}px;
   font-size: inherit;
+  outline: none;
 `
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,5 +1,18 @@
 import * as React from 'react'
 
+import { Box, Field, Button } from '../../components/uikit'
+
 export const Signup = () => {
-  return <div>Signup</div>
+  return (
+    <Box flex={1} flexbox="column" center>
+      <Box style={{width: '380px'}}>
+        <Field type="text" name="name" label="Name" mb={3} />
+        <Field type="text" name="email" label="E-mail" mb={3} />
+        <Field type="password" name="password" label="Password" mb={3} />
+        <Box flexbox center>
+        <Button>Sign Up</Button>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
