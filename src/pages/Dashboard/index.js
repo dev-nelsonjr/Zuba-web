acef3d0057eb82724a25e05bb25df2969083bc78
unknown <nelsonjr.dev@gmail.com>
2025-08-29 23:51:56 -0300

    feat: basic layout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,39 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { th } from '../../components/Theme'
 
-import { useAuth } from '../../components/Modules'
+// import { useAuth } from '../../components/Modules'
+
+// export const Dashboard = () => {
+//   const [auth, { signOut }] = useAuth()
+//   return (
+//     <div>
+//       Hello <strong>{auth.user.name}</strong> !{' '}
+//       <button onClick={signOut}> Sign out </button>
+//     </div>
+//   )
+// }
+
+const Container = styled('div')`
+  flex: 1;
+  display: flex;
+`
+const Menu = styled('aside')`
+  background: ${th.color('black')};
+  padding: ${th.space(2)}px;
+`
+
+const Main = styled('main')`
+  flex: 1;
+  max-width: 1200px;
+  margin: 0 auto;
+`
 
 export const Dashboard = () => {
-  const [auth, { signOut }] = useAuth()
   return (
-    <div>
-      Hello <strong>{auth.user.name}</strong> !{' '}
-      <button onClick={signOut}> Sign out </button>
-    </div>
+    <Container>
+      <Menu>Menu</Menu>
+      <Main>content</Main>
+    </Container>
   )
 }
