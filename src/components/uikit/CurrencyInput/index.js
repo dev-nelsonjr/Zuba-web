e13c021fc9015eba0144f2fee4b18ddea751ff5f
NelsonJr <nelsonjr.dev@gmail.com>
2025-09-26 22:09:52 -0300

    fix: extra zero on currency change value

--- src/components/uikit/CurrencyInput/index.js
+++ src/components/uikit/CurrencyInput/index.js
@@ -4,14 +4,17 @@ import { toMoney } from 'vanilla-masker'
 
 export const CurrencyInput = ({ value, onChange, ...props }) => {
   const handleChange = ev => {
-    const value = ev.target.value.replace('$', '')
-    onChange(value ? `${value}` : '0.00')
+    const value = ev.target.value.replace('R$', '').replace(',', '.').trim()
+    const currency = toMoney(value, { unit: 'R$', precision: 2 })
+    const newValue = currency.replace('R$', '').replace(',', '.').trim()
+
+    onChange(newValue ? `${newValue}` : '0.00')
   }
 
   return (
     <Input
       {...props}
-      value={toMoney(value, { unit: '$' })}
+      value={toMoney(value, { unit: '$', precision: 2 })}
       onChange={handleChange}
     />
   )
