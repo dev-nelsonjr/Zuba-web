e113d25246cb685e4796f6cabcb0400eb947c8bd
NelsonJr <nelsonjr.dev@gmail.com>
2025-11-07 19:54:16 -0300

    fix: format decimal currency values

--- src/components/atoms/Currency/index.js
+++ src/components/atoms/Currency/index.js
@@ -2,8 +2,14 @@ import * as React from 'react'
 import { toMoney } from 'vanilla-masker'
 import { Box } from '~/components/atoms/Box'
 
-export const Currency = ({ value, color, ...props }) => (
-  <Box {...props} color={color || (value < 0 ? 'red' : 'green')}>
-    {toMoney(value, { unit: '$' })}
-  </Box>
-)
+export const Currency = ({ value, color, ...props }) => {
+  const moneyValue = Number(value || 0)
+    .toFixed(2)
+    .replace('.', '')
+
+  return (
+    <Box {...props} color={color || (value < 0 ? 'red' : 'green')}>
+      {toMoney(moneyValue, { unit: '$' })}
+    </Box>
+  )
+}
