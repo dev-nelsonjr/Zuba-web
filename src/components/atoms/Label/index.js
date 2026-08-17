d58b828794502eb0ed880f2674502efe7a2fa15c
NelsonJr <nelsonjr.dev@gmail.com>
2026-08-17 20:42:16 -0300

    refactor: simplify label theme spacing

--- src/components/atoms/Label/index.js
+++ src/components/atoms/Label/index.js
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 import { themeGet } from '@styled-system/theme-get'
 
 export const Label = styled('label')`
-  padding: ${props => themeGet('space.2')}px ${props => themeGet('space.3')}px;
+  padding: ${themeGet('space.2')}px ${themeGet('space.3')}px;
 `
