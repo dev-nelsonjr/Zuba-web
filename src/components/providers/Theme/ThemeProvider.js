d360baf424f6eaf2364f027d1eef753cacabdaec
NelsonJr <nelsonjr.dev@gmail.com>
2026-08-22 13:44:29 -0300

    fix: prevent style props from reaching the DOM

--- src/components/providers/Theme/ThemeProvider.js
+++ src/components/providers/Theme/ThemeProvider.js
@@ -1,11 +1,17 @@
-import { ThemeProvider } from 'styled-components'
+import isPropValid from '@emotion/is-prop-valid'
+import { StyleSheetManager, ThemeProvider } from 'styled-components'
 
 import { definitions } from './definitions'
 import { GlobalStyle } from './GlobalStyle'
 
+const shouldForwardProp = (propName, target) =>
+  typeof target !== 'string' || isPropValid(propName)
+
 export const Theme = ({ children }) => (
-  <ThemeProvider theme={definitions}>
-    <GlobalStyle bg="raisinBlack" color="white" fontSize={3} />
-    {children}
-  </ThemeProvider>
+  <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+    <ThemeProvider theme={definitions}>
+      <GlobalStyle bg="raisinBlack" color="white" fontSize={3} />
+      {children}
+    </ThemeProvider>
+  </StyleSheetManager>
 )
